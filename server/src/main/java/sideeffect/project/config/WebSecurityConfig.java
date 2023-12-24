2a48a514827a89a610bc773809391cb828d63a52
kimminsu <xjfcnfw3@naver.com>
2023-12-24 17:33:42 +0900

    feat: 버전에 맞게 시큐리티 설정

--- server/src/main/java/sideeffect/project/config/WebSecurityConfig.java
+++ server/src/main/java/sideeffect/project/config/WebSecurityConfig.java
@@ -4,14 +4,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import sideeffect.project.security.*;
+import sideeffect.project.security.JwtFilter;
+import sideeffect.project.security.JwtTokenProvider;
+import sideeffect.project.security.LoginFailureHandler;
+import sideeffect.project.security.LoginSuccessHandler;
+import sideeffect.project.security.RefreshTokenProvider;
+import sideeffect.project.security.SecurityExceptionHandlerFilter;
+
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Slf4j
@@ -19,8 +26,6 @@ import sideeffect.project.security.*;
 public class WebSecurityConfig{
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final RefreshTokenProvider refreshTokenProvider;
-
 
     @Bean
     public LoginSuccessHandler loginSuccessHandler(RefreshTokenProvider refreshTokenProvider) {
@@ -35,27 +40,20 @@ public class WebSecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
-                .httpBasic().disable()
-                .csrf().disable()
-                .cors().and()
-                .authorizeRequests()
-                .requestMatchers("/api/user/join", "/api/user/mypage/**", "/api/user/duple/**", "/api/social/login").permitAll()
-            .requestMatchers(HttpMethod.POST, "/api/token/at-issue/**").permitAll()
-            .requestMatchers(HttpMethod.POST, "/**").authenticated()
-                .requestMatchers(HttpMethod.GET, "/api/free-boards/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/like/**").hasAnyRole("USER", "ADMIN")
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/user/join", "/api/user/mypage/**", "/api/user/duple/**", "/api/social/login")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/token/at-issue/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/**").authenticated()
+                                .requestMatchers(HttpMethod.GET, "/api/free-boards/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/like/**").hasAnyRole("USER", "ADMIN")
+                        )
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new SecurityExceptionHandlerFilter(), JwtFilter.class)
-//                .formLogin()
-//                    .loginProcessingUrl("/api/user/login")
-//                    .usernameParameter("email")
-//                    .successHandler(loginSuccessHandler(refreshTokenProvider))
-//                    .failureHandler(loginFailureHandler())
-//                    .and()
                 .build();
     }
 }
